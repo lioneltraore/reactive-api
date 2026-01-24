ff3ccbd8798455b8c96fc74bf9403a4bf2cfc7af
Lionel Traore <hello@lioneltraore.com>
2026-01-24 09:05:20 +0000

    add basic controller methods

---
+++ src/main/java/com/gexcode/reactive/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.gexcode.reactive.controller;
+
+import com.gexcode.reactive.model.entity.User;
+import com.gexcode.reactive.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    public Mono<User> createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public Flux<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<User> getUserByID(@PathVariable String id) {
+        return userRepository.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteUser(@PathVariable String id) {
+        return userRepository.deleteById(id);
+    }
+}
